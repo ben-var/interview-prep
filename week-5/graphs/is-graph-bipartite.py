5c127e80c687ed6533334ebd300832de81b49279
ben-var <ben.var@outlook.com>
2019-06-09 14:20:01 -0500

    corrected implementation of is-graph-bipartite

--- week-5/graphs/is-graph-bipartite.py
+++ week-5/graphs/is-graph-bipartite.py
@@ -1,36 +1,23 @@
 class Solution:
-        
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        # run DFS with alternating sets adding each node. if a node 
-        # is tried to be added to a set in which
-        # it already exists in, then return false
-        #
-        # otherwise, return true
-        
-        A, B = set(), set()
-        
-        inA = True
-        for src, neighbors in enumerate(graph):
-            print ('src, neigh', src, neighbors)
-            if inA:
-                if src in B:
-                    return False
-                A.add(src)
-            else:
-                if src in A:
-                    return False
-                B.add(src)
-                
-            inA = not inA
-                    
-            for tgt in neighbors:
-                if inA:
-                    if tgt in B:
-                        return False
-                    A.add(tgt)
-                else:
-                    if tgt in A:
-                        return False
-                    B.add(tgt)
 
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+
+        colorings = {}
+
+        def dfsColor(pos):
+            for i in graph[pos]:
+                if i in colorings:
+                    if colorings[i] == colorings[pos]:
+                        return False
+                else:
+                    colorings[i] = not colorings[pos]
+                    if not dfsColor(i):
+                        return False
+            return True
+
+        for i in range(len(graph)):
+            if i not in colorings:
+                colorings[i] = True
+                if not dfsColor(i):
+                    return False
         return True
